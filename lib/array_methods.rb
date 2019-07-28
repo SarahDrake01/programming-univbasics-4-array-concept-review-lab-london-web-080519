e20773d2c6b598c4fd26a5cc364879082ce38cb5
Sarah Drake <jussaradrake@outlook.com>
2019-07-28 10:42:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  new_array = []
+  new_array = [1, 2]
   first_element = new_array[0]
   array = []
   counter = 0
@@ -7,7 +7,7 @@ while counter < array.length do
       new_array << (array[counter])
       counter +=1
   end
-  return new_array[]
+  return new_array[0]
 end
 
 
